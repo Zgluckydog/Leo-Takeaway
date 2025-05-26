4a7901f74c2cec2038d7b983459cbca697ef2b92
Zgluckydog <2505979936@qq.com>
2025-05-26 10:25:41 +0800

    shop status operator

--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -1,40 +1,30 @@
-package com.sky.controller.admin;
+package com.sky.controller.user;
 
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("adminShopController")
-@RequestMapping("/admin/shop")
+@RestController("userShopController")
+@RequestMapping("/user/shop")
 @Api(tags = "店铺相关接口")
 @Slf4j
 public class ShopController {
     @Autowired
     private RedisTemplate redisTemplate;
+
     public static final String KEY = "SHOP_STATUS";
-    /**
-     * 设置店铺营业状态
-     * */
-    @PutMapping("/{status}")
-    @ApiOperation("设置店铺营业状态")
-    public Result setStatus(@PathVariable Integer status) {
-        log.info("设置店铺营业状态：{}",status==1?"营业中":"打烊中");
-        redisTemplate.opsForValue().set(KEY, status);
-        return Result.success();
-    }
 
     /**
      * 查询店铺状态
      * */
     @GetMapping("/status")
     @ApiOperation("获取营业状态")
-    public Result getStatus() {
-        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get("SHOP_STATUS");
         log.info("获取到店铺的营业状态为：{}",status==1?"营业中":"打烊中");
         return Result.success(status);
     }
